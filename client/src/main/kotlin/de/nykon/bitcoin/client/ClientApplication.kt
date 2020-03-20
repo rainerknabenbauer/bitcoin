bfc6e0b7455c7e5eb62d9433b8e0ff39ad107f55
Rainer Knabenbauer <rainer.knabenbauer@posteo.de>
2020-03-20 11:07:38 +0100

    Quick hack to keep to programming toggeling every 5 minuten

--- client/src/main/kotlin/de/nykon/bitcoin/client/ClientApplication.kt
+++ client/src/main/kotlin/de/nykon/bitcoin/client/ClientApplication.kt
@@ -7,12 +7,19 @@ fun main(args : Array<String>) {
     if (args.size < 2) {
         println("Set on start: [1] ApiKey\n[2] ApiSecret")
     } else {
-        val bitcoinDeClient = BitcoinDeClient()
-        val offers = bitcoinDeClient.getOffers(args[0], args[1])
 
-        if (!offers.isNullOrEmpty()) {
-            val backendClient = BackendClient()
-            backendClient.persist(offers)
+        while (true) {
+            val bitcoinDeClient = BitcoinDeClient()
+            val offers = bitcoinDeClient.getOffers(args[0], args[1])
+
+            if (!offers.isNullOrEmpty()) {
+                val backendClient = BackendClient()
+                backendClient.persist(offers)
+            }
+
+            Thread.sleep(300000)
         }
+
+
     }
 }
