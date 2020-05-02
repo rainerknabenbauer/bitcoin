8dc730441350e36c3168118ac8213e33057e86c4
Rainer Knabenbauer <rainer.knabenbauer@posteo.de>
2020-05-02 09:35:55 +0200

    Add price for automatic selling

--- backend/src/main/kotlin/de/nykon/bitcoin/backend/trade/seller/SellerSchedulConfig.kt
+++ backend/src/main/kotlin/de/nykon/bitcoin/backend/trade/seller/SellerSchedulConfig.kt
@@ -1,6 +1,7 @@
 package de.nykon.bitcoin.backend.trade.seller
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
+import java.math.BigDecimal
 
 @Configuration
 open class SellerSchedulConfig {
@@ -10,5 +11,8 @@ open class SellerSchedulConfig {
 
     @Value(value = "\${api.isLive}")
     var isLiveChange: Boolean = false
+
+    @Value(value = "\${bitcoin.trading.selling.start}")
+    var threshold: BigDecimal = BigDecimal.ZERO
 
 }
